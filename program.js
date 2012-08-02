7b0e8cf6409b9af57192057fbd0605b271e123ef
Cory Fowler <syntaxc4@gmail.com>
2012-08-02 11:21:52 -0700

    Added Read Service Bus, ensured package.json works

---
+++ program.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var azure = require('azure');
+
+var namespace = '<sb-namespace>';
+var issuer = '<sb-issuer>';
+var accessKey = '<sb-access-key>';
+
+program.version('0.0.1')
+	.parse(process.argv);
+
+var loop = setInterval(function() {
+		var serviceBus = azure.createServiceBusService(namespace, accessKey, issuer);
+		serviceBus.receiveQueueMessage('sbmessageme', function(error, serverMessage){
+			if(!error){
+				console.log(serverMessage.body);
+			}
+		});
+	   }, 10);
+
