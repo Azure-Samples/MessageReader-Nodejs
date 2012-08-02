d03a2bf4a267313242e0936da288240ca0812d98
Cory Fowler <syntaxc4@gmail.com>
2012-08-02 16:19:16 -0700

    moved queue name into a variable, added demo.md file for walk through instructions

--- code/src/app.js
+++ code/src/app.js
@@ -6,13 +6,14 @@ var azure = require('azure');
 var namespace = '<sb-namespace>';
 var issuer = '<sb-issuer>';
 var accessKey = '<sb-access-key>';
+var queue = '<sb-queue-name>';
 
 program.version('0.0.1')
 	.parse(process.argv);
 
 var loop = setInterval(function() {
 		var serviceBus = azure.createServiceBusService(namespace, accessKey, issuer);
-		serviceBus.receiveQueueMessage('sbmessageme', function(error, serverMessage){
+		serviceBus.receiveQueueMessage(queue, function(error, serverMessage){
 			if(!error){
 				console.log(serverMessage.body);
 			}
