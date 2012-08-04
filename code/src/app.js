08b204b628f55620eadba9cee4413c6060a7553c
Cory Fowler <syntaxc4@gmail.com>
2012-08-04 02:33:37 -0700

    moved service bus variables to external config file

--- code/src/app.js
+++ code/src/app.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 
 var program = require('commander');
-var azure = require('azure');
-
-var namespace = '<sb-namespace>';
-var issuer = '<sb-issuer>';
-var accessKey = '<sb-access-key>';
-var queue = '<sb-queue-name>';
+var azure   = require('azure');
+var config  = require('./config');
 
 program.version('0.0.1')
 	.parse(process.argv);
 
 var loop = setInterval(function() {
-		var serviceBus = azure.createServiceBusService(namespace, accessKey, issuer);
-		serviceBus.receiveQueueMessage(queue, function(error, serverMessage){
+		var serviceBus = azure.createServiceBusService(config.namespace, config.accessKey, config.issuer);
+		serviceBus.receiveQueueMessage(config.queue, function(error, serverMessage){
 			if(!error){
 				console.log(serverMessage.body);
 			}
